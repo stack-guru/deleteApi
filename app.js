500b7a68c6045d87dcccdc92678e204d77befac3
Full Stack Developer <GoldenHand0302@gmail.com>
2023-08-25 18:16:46 -0400

    [Update] delete to post

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
@@ -23,14 +23,12 @@ app.use(function(req, res, next) {
 });
 
 app.get("/", (req, res) => {
-    res.status(201).send("Hello World");
+  res.status(201).send("Hello World");
 })
 
-app.delete("/collection", async (req, res) => {
-    console.log('delete collection = ', req.body.token, req.body.network, req.body.platform)
-    const deleted = await Collection.deleteOne({ token: req.body.token, network: req.body.network, platform: {'$regex': `^${req.body.platform}$`, $options: 'i'}})
-    console.log('deleted collection ', deleted)
-    res.status(201).send(deleted)
+app.post("/collection/delete", async (req, res) => {
+  const deleted = await Collection.deleteOne({ token: req.body.token, network: req.body.network, platform: { '$regex': `^${req.body.platform}$`, $options: 'i' } })
+  res.status(201).send(deleted)
 })
 
 app.listen(PORT, function () {
